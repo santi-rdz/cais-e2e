2c5196856ac30c123f13c4e8526e50a377bb91e6
RDZ <raul.rodriguez39@uabc.edu.mx>
2026-05-25 13:05:05 -0700

    feat: login page created

---
+++ demo/src/main/java/com/Pages/LoginPage.java
@@ -0,0 +1,19 @@
+package com.Pages;
+
+import org.openqa.selenium.By;
+
+import com.Base.BasePage;
+
+public class LoginPage extends BasePage {
+  // locators
+  By emailInput = By.id("email");
+  By passwordInput = By.id("password");
+  By submit = By.cssSelector("[data-testid='login-submit']");
+
+  // actions
+  public void loginAs(String email, String password){
+    type(emailInput, email);
+    type(passwordInput, password);
+    click(submit);
+  }
+}
