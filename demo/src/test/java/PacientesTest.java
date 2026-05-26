5e4b7bfc1e879617a64a47e3a1fbc54492b79571
El Algo <60892992+el-algo@users.noreply.github.com>
2026-05-26 22:54:36 +0000

    fix: BeforeMethod for PatientesTest

--- demo/src/test/java/PacientesTest.java
+++ demo/src/test/java/PacientesTest.java
@@ -1,6 +1,7 @@
 import Base.BaseTest;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.Pages.EditPage;
@@ -15,14 +16,17 @@ public class PacientesTest extends BaseTest {
     String testBirthDay = "10/05/2001";
     String testTelephoneNumber = "6648995013";
     String testEditedTelephoneNumber = "6648998888";
-
     String testFakeName = "Rafaelino";
 
-    @Test(description = "register new patient to Pacientes")
-    public void registerNewPatient() {
+    @BeforeMethod
+    public void setUp() {
         pacientesPage = new PacientesPage();
         pacientesPage.openFromSidebar();
         Assert.assertTrue(pacientesPage.getCurrentUrl().contains("/pacientes"));
+    }
+
+    @Test(description = "register new patient to Pacientes")
+    public void registerNewPatient() {
         pacientesPage.addNewPatient();
         Assert.assertEquals(pacientesPage.getModalTitle(), "Registro de Nuevo Paciente");
         pacientesPage.filloutNewPatient(testName, testLastName, testBirthDay, testTelephoneNumber);
@@ -31,9 +35,6 @@ public class PacientesTest extends BaseTest {
 
     @Test(description = "edit patient data")
     public void editPatientData() {
-        pacientesPage = new PacientesPage();
-        pacientesPage.openFromSidebar();
-        Assert.assertTrue(pacientesPage.getCurrentUrl().contains("/pacientes"));
         pacientesPage.openFirstPatientByName(fullName);
 
         EditPage editPage = new EditPage();
@@ -46,36 +47,24 @@ public class PacientesTest extends BaseTest {
 
     @Test(description = "search for a specific patient")
     public void searchForPatient() {
-        pacientesPage = new PacientesPage();
-        pacientesPage.openFromSidebar();
-        Assert.assertTrue(pacientesPage.getCurrentUrl().contains("/pacientes"));
         pacientesPage.searchForPatient(testName);
         Assert.assertTrue(pacientesPage.isNewPatientOnPatientList(fullName));
     }
 
     @Test(description = "search for a non-existent patient")
     public void searchForNonExistentPatient() {
-        pacientesPage = new PacientesPage();
-        pacientesPage.openFromSidebar();
-        Assert.assertTrue(pacientesPage.getCurrentUrl().contains("/pacientes"));
         pacientesPage.searchForPatient(testFakeName);
         Assert.assertTrue(pacientesPage.isNoDataVisible());
     }
 
     @Test(description = "sort patient list by age (oldest to youngest)")
     public void sortPatientListOldestToYoungest() {
-        pacientesPage = new PacientesPage();
-        pacientesPage.openFromSidebar();
-        Assert.assertTrue(pacientesPage.getCurrentUrl().contains("/pacientes"));
         pacientesPage.selectSortByOldestToYoungest();
         Assert.assertTrue(pacientesPage.isSortedOldestToYoungest());
     }
 
     @Test(description = "filter patient list to show only masculine patients")
     public void sortPatientListYoungestToOldest() {
-        pacientesPage = new PacientesPage();
-        pacientesPage.openFromSidebar();
-        Assert.assertTrue(pacientesPage.getCurrentUrl().contains("/pacientes"));
         pacientesPage.seletcFilterByMasculine();
         Assert.assertTrue(pacientesPage.isShownOnlyMasculine());
     }
